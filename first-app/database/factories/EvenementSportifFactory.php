f09db5b9edbafe5959b1c1b8b5a5bd30bec0ac28
Mohamed <ahnbi32@gmail.com>
2023-05-04 13:02:47 +0200

    adding image

--- first-app/database/factories/EvenementSportifFactory.php
+++ first-app/database/factories/EvenementSportifFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\EvenementSportif>
@@ -18,12 +19,13 @@ class EvenementSportifFactory extends Factory
     {
         $width=200;
         $height=200;
-        $path=$this->faker->image('storage/images' ,$width, $height, 'person', true, true, 'person', false);
+        $path=$this->faker->image('storage/app/public/images' ,$width, $height, 'person', true, true, 'person', false);
         return [
             'nom' => $this->faker->sentence(),
             'description' => $this->faker->words(2,true),
             'lieu'=> $this->faker->state(),
-            'poster' => $path,//$this->faker->imageUrl(360, 360, true),
+            'poster' =>$path,// $this->faker->imageUrl(360, 360, true),
+            'posterURL'=>config('app.url').'/storage/'.Str::after($path, 'public/'),
             'dateDebut' => $this->faker->date(),
             'dateFin' => $this->faker->date(),
         ];
